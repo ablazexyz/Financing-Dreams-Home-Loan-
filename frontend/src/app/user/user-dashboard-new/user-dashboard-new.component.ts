f2e5ce5dda1aaf68533382ba80cd4da12f3055d9
Abarpan3 <abarpan3@gmail.com>
2020-10-01 12:19:03 +0530

    Day 2 Frontend

--- frontend/src/app/user/user-dashboard-new/user-dashboard-new.component.ts
+++ frontend/src/app/user/user-dashboard-new/user-dashboard-new.component.ts
@@ -11,12 +11,6 @@ export class UserDashboardNewComponent implements OnInit {
   name: string;
 
   constructor(private router: Router, private service:UserService) {
-
-    this.service.getUserDetails(sessionStorage.getItem('username')).subscribe(data=>{
-
-      sessionStorage.setItem('Name',(data.firstName+' '+data.lastName));
-
-    });
   }
 
   ngOnInit(): void {
