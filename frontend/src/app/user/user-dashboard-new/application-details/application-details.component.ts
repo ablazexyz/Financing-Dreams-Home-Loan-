65faa9c24cdb44d7bb245135e4679bd34acc5db5
Tanuj Chakraborty <tans.c2@gmail.com>
2020-10-03 12:52:26 +0530

    improved validation on application details form

--- frontend/src/app/user/user-dashboard-new/application-details/application-details.component.ts
+++ frontend/src/app/user/user-dashboard-new/application-details/application-details.component.ts
@@ -1,22 +1,23 @@
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, ValidatorFn, FormControl, AbstractControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ValidatorFn,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user.service';
 import { CustomerDetails } from '../../customerDetails';
 import { Application } from '../../application';
-
-
 
 @Component({
   selector: 'application-details',
   templateUrl: './application-details.component.html',
   styleUrls: ['./application-details.component.css'],
 })
-
-
 export class ApplicationDetailsComponent implements OnInit {
-
-
   salary: number;
 
   userDetail: CustomerDetails;
@@ -25,40 +26,49 @@ export class ApplicationDetailsComponent implements OnInit {
 
   applicationdetails: Application;
 
-  constructor(private fb: FormBuilder, private router: Router, private service: UserService) {
-    if (!sessionStorage.getItem('username')){
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private service: UserService
+  ) {
+    if (!sessionStorage.getItem('username')) {
       this.router.navigate(['/userLogin']);
     }
 
-
-    this.service.getUserDetails(sessionStorage.getItem('username')).subscribe(data=>{
-
-      this.salary = 6;
-      console.log(this.salary);
-    });
-
-
+    this.service
+      .getUserDetails(sessionStorage.getItem('username'))
+      .subscribe((data) => {
+        this.salary = 6;
+        console.log(this.salary);
+      });
   }
 
-
   ngOnInit(): void {
-    this.applicationDetailsForm = this.fb.group({
-      property_location: ['', Validators.required],
-      property_name: ['', Validators.required],
-      estimated_property_amt: [
-        '',
-        [Validators.required, Validators.pattern('^[0-9]*$')],
-      ],
-      loan_amount: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-      interest_rate: [
-        '',
-        [Validators.required, Validators.pattern('^[0-9]*$')],
-      ],
-      tenure: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-      LOA: [],
-      NOC: [],
-      agreement_to_sale: [],
-    }/*, { validator: this.validateAmount}*/);
+    this.applicationDetailsForm = this.fb.group(
+      {
+        property_location: ['', Validators.required],
+        property_name: ['', Validators.required],
+        estimated_property_amt: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]*$')],
+        ],
+        loan_amount: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]*$')],
+        ],
+        interest_rate: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(new RegExp(/^\d*(?:[.,]\d{1,2})?$/)),
+          ],
+        ],
+        tenure: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+        LOA: [],
+        NOC: [],
+        agreement_to_sale: [],
+      } /*, { validator: this.validateAmount}*/
+    );
   }
   /*
   validateAmount: ValidatorFn = (fg: FormGroup) => {
@@ -69,20 +79,23 @@ export class ApplicationDetailsComponent implements OnInit {
   }
   */
   addApplicationDetails(): void {
+    this.applicationdetails = new Application(
+      this.applicationDetailsForm.controls.property_location.value,
+      this.applicationDetailsForm.controls.property_name.value,
+      this.applicationDetailsForm.controls.estimated_property_amt.value,
+      this.applicationDetailsForm.controls.loan_amount.value,
+      this.applicationDetailsForm.controls.interest_rate.value,
+      this.applicationDetailsForm.controls.tenure.value
+    );
 
-    this.applicationdetails = new Application(this.applicationDetailsForm.controls.property_location.value,
-                                              this.applicationDetailsForm.controls.property_name.value,
-                                              this.applicationDetailsForm.controls.estimated_property_amt.value,
-                                              this.applicationDetailsForm.controls.loan_amount.value,
-                                              this.applicationDetailsForm.controls.interest_rate.value,
-                                              this.applicationDetailsForm.controls.tenure.value);
-
-
-    this.service.addApplication(sessionStorage.getItem('username'),this.applicationdetails).subscribe(data=>{
-
-      this.applicationdetails = data;
-      console.log(this.applicationdetails);
-
-    })
+    this.service
+      .addApplication(
+        sessionStorage.getItem('username'),
+        this.applicationdetails
+      )
+      .subscribe((data) => {
+        this.applicationdetails = data;
+        console.log(this.applicationdetails);
+      });
   }
 }
