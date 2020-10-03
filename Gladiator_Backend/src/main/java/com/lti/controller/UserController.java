f906655faf4871d3e4448331aff64b78c3da7eec
Tanuj Chakraborty <tans.c2@gmail.com>
2020-10-03 18:46:08 +0530

    added exception handler to handle large file upload requests

--- Gladiator_Backend/src/main/java/com/lti/controller/UserController.java
+++ Gladiator_Backend/src/main/java/com/lti/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.lti.customExceptions.FileTypeException;
@@ -211,6 +213,14 @@ public class UserController {
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
 		}
 	}
+	
+	
+	//Function to handle large file upload requests
+		@ExceptionHandler(MaxUploadSizeExceededException.class)
+		public ResponseEntity<String> handleFileSizeException() {
+			String message = "File size too large!";
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+		}
 
 
 }
