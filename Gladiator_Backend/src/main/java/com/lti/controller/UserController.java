81ddc8f93b6a46ee3096af93c9914a7460103b6f
unknown <abarpan3@gmail.com>
2020-10-04 12:21:33 +0530

    Day 5 Email & OTP Service Backend

--- Gladiator_Backend/src/main/java/com/lti/controller/UserController.java
+++ Gladiator_Backend/src/main/java/com/lti/controller/UserController.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +31,7 @@ import com.lti.model.Customer_Details;
 import com.lti.model.Login;
 import com.lti.model.Registration;
 import com.lti.service.CustomerService;
+import com.lti.service.MailService;
 
 @RestController
 @CrossOrigin
@@ -39,9 +41,10 @@ public class UserController {
 
 	@Autowired
 	private CustomerService service;
-	
-	private final Path rootLocation = Paths
-			.get("D:\\LTI_TRAINING\\gladiator\\MyBackendExperiments\\Fileupload-Example\\storedFiles");
+
+	private MailService mail;
+
+	private final Path rootLocation = Paths.get("C:\\Gladiator\\Files Upload");
 
 	// http://localhost:9091/HomeApp/users/adlogin
 	@PostMapping(path = "adlogin")
@@ -74,11 +77,46 @@ public class UserController {
 		try {
 			service.findRegistrationDetailsbyEmail(reg.getEmailId());
 			return ResponseEntity.notFound().build();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			service.createRegistration(reg);
+
+			String msg = "";
+			msg += ("Welcome to Financing Dreams Home Loans, " + reg.getFirstName() + " " + reg.getLastName()
+					+ ".Thank You For Registering with us. " + "Please login to apply for your first Loan Today");
+
+			MailService.send(reg.getEmailId(), "Registration Successful", msg);
 		}
 		return null;
+	}
+
+	// http://localhost:9091/HomeApp/users/forgot/{emailId}
+	@GetMapping(path = "forgot/{emailId}")
+	public String forgotPassword(@PathVariable("emailId") String email) {
+
+		String numbers = "0123456789";
+
+		Random rndm_method = new Random();
+
+		char[] otp = new char[4];
+
+		for (int i = 0; i < 4; i++) {
+
+			otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+		}
+		
+		String pass = String.valueOf(otp);
+
+		String msg = "";
+		msg += ("Your OTP for Password Reset is: "+pass);
+
+		MailService.send(email, "OTP For Password Reset", msg);
+		return pass;
+	}
+	
+	@GetMapping(path="updatePass")
+	public Registration updatePassword(@RequestBody Registration reg) {
+		
+		return service.modifyRegistration(reg);
 	}
 
 	@GetMapping(path = "/")
@@ -86,104 +124,102 @@ public class UserController {
 
 		return service.findAllRegistrations();
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/isFirstTimeUser/{emailId}
-	@GetMapping(path= "isFirstTimeUser/{emailId}")
+	@GetMapping(path = "isFirstTimeUser/{emailId}")
 	public Registration getRegistrationDetails(@PathVariable("emailId") String email) {
-		
+
 		return service.findRegistrationDetailsbyEmail(email);
 	}
-	
+
 	@PostMapping(path = "customerdetails/{emailId}")
-	public Registration setCustomerDetails(@PathVariable("emailId") String email,@RequestBody Customer_Details cd) {
-		
+	public Registration setCustomerDetails(@PathVariable("emailId") String email, @RequestBody Customer_Details cd) {
+
 		Registration reg = service.findRegistrationDetailsbyEmail(email);
 		reg.setCdetails(cd);
 		return service.modifyRegistration(reg);
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/customerdetails/{emailId}
-	@GetMapping(path= "customerdetails/{emailId}")
+	@GetMapping(path = "customerdetails/{emailId}")
 	public Customer_Details getCustomerDetails(@PathVariable("emailId") String email) {
-		
+
 		try {
-			Customer_Details cd =  service.findCustomerDetailsbyEmail(email);
+			Customer_Details cd = service.findCustomerDetailsbyEmail(email);
 			return cd;
-		}
-		catch(Exception e) {
-			return null; 
+		} catch (Exception e) {
+			return null;
 		}
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/applicationdetails/{emailId}
 	@PostMapping(path = "applicationdetails/{emailId}")
-	public Application addApplication(@PathVariable("emailId") String email,@RequestBody Application appl) {
-		
+	public Application addApplication(@PathVariable("emailId") String email, @RequestBody Application appl) {
+
 		Customer_Details cd = service.findCustomerDetailsbyEmail(email);
-		
+
 		appl.setCdetails2(cd);
 		appl.setLoanStatus("Pending");
-	
+
 		if (cd.getApplications().isEmpty()) {
 			cd.setApplications(new HashSet<Application>());
 		}
-		
+
 		cd.addApplications(appl);
-		
+
 		service.createApplication(appl);
-		
+
 		return appl;
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/applicationdetails/{emailId}
-	@GetMapping(path= "applicationdetails/{emailId}")
-	public List<Application> getAllApplicationsbyEmail(@PathVariable("emailId") String email){
-		
+	@GetMapping(path = "applicationdetails/{emailId}")
+	public List<Application> getAllApplicationsbyEmail(@PathVariable("emailId") String email) {
+
 		return service.findAllApplicationsbyEmail(email);
-		
+
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/application/{id}
-	@GetMapping(path= "application/{id}")
+	@GetMapping(path = "application/{id}")
 	public Application getApplicationById(@PathVariable("id") int applId) {
-		
+
 		return service.findApplicationById(applId);
 	}
-	
+
 	// http://localhost:9091/HomeApp/users/account/{emailid}
 	@GetMapping(path = "account/{email}")
 	public Account getAccountByEmail(@PathVariable("email") String email) {
-		
+
 		try {
 			return service.findAccountByEmail(email);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
-		
+
 	}
-	
+
 	@PostMapping("/fileUpload/{id}/{documentType}")
 	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file,
 			@PathVariable(name = "id") String id, @PathVariable(name = "documentType") String documentType) {
-		// Path where the file will be stored(will create a directory named with whatever is passed to 'id')
-		Path uploadPath = Path.of(rootLocation.toString(), id);
+		// Path where the file will be stored(will create a directory named with
+		// whatever is passed to 'id')
+		Path uploadPath = Paths.get(rootLocation.toString(), id);
 		// Checking if the folder where to upload exists or not
 		File userDocumentsFolder = new File(uploadPath.toString());
-		
-		//allow only pdf uploads
+
+		// allow only pdf uploads
 		String fileContentType = file.getContentType();
 		try {
 			if (!fileContentType.equals("application/pdf")) {
 				throw new FileTypeException();
-			} 
-		}catch (FileTypeException e) {
+			}
+		} catch (FileTypeException e) {
 			return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(e.getMessage());
 		}
 		boolean folderCreated = false;
-		//if it doesn't exist , create that folder
-		
-		
+		// if it doesn't exist , create that folder
+
 		if (!userDocumentsFolder.exists()) {
 			if (userDocumentsFolder.mkdir()) {
 				System.out.println("created directory");
@@ -191,36 +227,35 @@ public class UserController {
 			} else {
 				System.out.println("Failed to create directory");
 			}
-		}		
+		}
 
 		String message;
 		try {
 			try {
-				//Files.copy(file.getInputStream(), uploadPath.resolve(file.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
-				//Files.copy(file.getInputStream(), uploadPath.resolve("resume.pdf"),StandardCopyOption.REPLACE_EXISTING);
-				Files.copy(file.getInputStream(), uploadPath.resolve(documentType + ".pdf"),StandardCopyOption.REPLACE_EXISTING);
-			} 
-			catch (Exception e) {
+				// Files.copy(file.getInputStream(),
+				// uploadPath.resolve(file.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
+				// Files.copy(file.getInputStream(),
+				// uploadPath.resolve("resume.pdf"),StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(file.getInputStream(), uploadPath.resolve(documentType + ".pdf"),
+						StandardCopyOption.REPLACE_EXISTING);
+			} catch (Exception e) {
 				throw new RuntimeException("FAIL!");
-			} 
-			//files.add(file.getOriginalFilename());
+			}
+			// files.add(file.getOriginalFilename());
 
 			message = "Successfully uploaded!";
 			return ResponseEntity.status(HttpStatus.OK).body(message);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			message = "Failed to upload!";
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
 		}
 	}
-	
-	
-	//Function to handle large file upload requests
-		@ExceptionHandler(MaxUploadSizeExceededException.class)
-		public ResponseEntity<String> handleFileSizeException() {
-			String message = "File size too large!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
-		}
 
+	// Function to handle large file upload requests
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<String> handleFileSizeException() {
+		String message = "File size too large!";
+		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+	}
 
 }
