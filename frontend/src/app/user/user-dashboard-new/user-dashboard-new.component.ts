269762883e379673ff20401c511e87e98bed5f62
ablazexyz <dineshthakurab@gmail.com>
2020-10-06 08:11:40 +0530

    created user profile modal

--- frontend/src/app/user/user-dashboard-new/user-dashboard-new.component.ts
+++ frontend/src/app/user/user-dashboard-new/user-dashboard-new.component.ts
@@ -1,3 +1,4 @@
+import { Register } from './../register';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
@@ -10,11 +11,18 @@ import { UserService } from '../user.service';
 export class UserDashboardNewComponent implements OnInit {
 
   user: string;
+  registrationDetails: Register;
   constructor(private router: Router, private service:UserService) {
   }
 
   ngOnInit(): void {
     this.user = sessionStorage.getItem('username');
+    this.service.getRegDetails(sessionStorage.getItem('username')).subscribe(data => {
+
+      this.registrationDetails = data;
+      console.log(this.registrationDetails);
+      // console.log('Registration Details :', data);
+    });
   }
 
   SignOut(): void{
