2c02a163b8a786aca4f15a3596528cca51b7780a
ablazexyz <dineshthakurab@gmail.com>
2020-10-07 17:12:33 +0530

    UI changes in Homepage

---
+++ frontend/src/app/documents-required/documents-required.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-documents-required',
+  templateUrl: './documents-required.component.html',
+  styleUrls: ['./documents-required.component.css']
+})
+export class DocumentsRequiredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
